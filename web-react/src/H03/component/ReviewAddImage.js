2a7a88e8f686dcbc11ec87934b0658ad85c6f328
Hyewon23 <ch2w2006@kookmin.ac.kr>
2020-11-23 11:37:11 +0900

    [H03_ReviewPage]update_ReviewAddImage

--- web-react/src/H03/component/ReviewAddImage.js
+++ web-react/src/H03/component/ReviewAddImage.js
@@ -2,7 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import photo1 from '../img/photo1.png';
 import photo2 from '../img/photo2.png';
-import food from '../img/food.png'
+import food from '../img/food.png';
+import xButton from '../img/xButton.png';
 
 const Wrap = styled.div`
 `;
@@ -28,19 +29,13 @@ const AddImage = styled.button`
     }
     #photo2{
         position: absolute;
-        // width: 6.4px;
-        // height: 6.67px;
         left: 47.8px;
         top:37.33px;
-
-        // background: #333333;
     }
 `;
 
 const AddText = styled.div`
     position: absolute;
-    // width: 102px;
-    // height: 17px;
     left: 25px;
     top: 60px;
     
@@ -49,8 +44,6 @@ const AddText = styled.div`
     font-weight: 500;
     font-size: 12px;
     line-height: 14px;
-    // display: flex;
-    // align-items: center;
     text-align: center;
     
     color: #333333;
@@ -68,20 +61,12 @@ const ReviewImage = styled.span`
 
 const AddX = styled.div`
     position: absolute;
-    width: 8px;
-    height: 10px;
-    top: 255px;
-    
-    font-family: Roboto;
-    font-style: normal;
-    font-weight: normal;
-    font-size: 10px;
-    line-height: 12px;
-    display: flex;
-    align-items: center;
-    text-align: center;
-    
-    color: #FFFFFF;
+    width: 12px;
+    height: 12px;
+    left: 223px;
+    top: 254px;
+
+    background: url(${xButton});
 `;
 
 function ReviewAddImage(){
@@ -94,9 +79,9 @@ function ReviewAddImage(){
             </AddImage>
 
             <ReviewImage style={{left: 137}}/>
-            <AddX style={{left: 223}}>x</AddX>
+            <AddX style={{left: 221}}/>
             <ReviewImage style={{left: 250}}/>
-            <AddX style={{left: 336}}>x</AddX>
+            <AddX style={{left: 334}}/>
         </Wrap>
     )
 }
