ed6a4b07f22e9767a69a3adb3b8b5389818b89a1
Hyewon23 <ch2w2006@kookmin.ac.kr>
2021-01-16 21:52:59 +0900

    [TicTacToe]condition basic

--- web-react/src/TicTacToe/component/Board.js
+++ web-react/src/TicTacToe/component/Board.js
@@ -1,9 +1,13 @@
 import React, {useState, useEffect} from 'react';
 import './Board.css';
+import {Condition} from "./Condition";
 
 const value = [0,0,0,0,0,0,0,0,0];
+const choose_X = [0,0,0,0,0,0,0,0,0];
+const choose_O = [0,0,0,0,0,0,0,0,0];
 
 export const Board = (props) => {
+    let result = "";
     const [user, setUser] = useState({
         player : 'X',
         count : 0,
@@ -11,7 +15,7 @@ export const Board = (props) => {
 
     const btnClick = (idx) => {
         if (value[idx] === 0){
-            console.log("어떤 칸을 눌렀나요?")
+            console.log("어떤 칸을 눌렀나요?");
             // user X = 1, user O = 2를 부여
             if (user.count % 2 === 0){
                 setUser({
@@ -19,6 +23,9 @@ export const Board = (props) => {
                     count : user.count+1,
                 })
                 value[idx] = 1;
+                choose_X[idx] = 1;
+                result = Condition(user.count, choose_X);
+                console.log(user.player,result);
             }
             else{
                 setUser({
@@ -26,9 +33,13 @@ export const Board = (props) => {
                     count : user.count+1,
                 })
                 value[idx] = 2;
+                choose_O[idx] = 1;
+                result = Condition(user.count, choose_O);
+                console.log(user.player, result);
             }
-            console.log(user.count, "번째: ", user.player,"가 ", idx, "번째 칸을 눌렀습니다!ㅎㅎ");
-            console.log(value);
+            // console.log(user.count, "번째: ", user.player,"가 ", idx, "번째 칸을 눌렀습니다!ㅎㅎ");
+            // console.log(value);
+            // console.log(choose_X, choose_O);
         }
         else{
             console.log(idx,"번째 칸은 이미 누르셨습니다ㅜ-ㅜ");
@@ -36,29 +47,25 @@ export const Board = (props) => {
         return value;
     }
 
-    const DisplayUser = (props) => {
-        if (value[props]===1) return 'X';
-        else if (value[props]===2) return 'O';
-    }
     return <>
         <div>
             <table>
                 <tbody>
-                <tr>
-                    <td><button className="block" onClick={() => {btnClick(0)}}>{value[0]===0? '': value[0]===1? 'X':'O'}</button></td>
-                    <td><button className="block" onClick={() => {btnClick(1)}}>{value[1]===0? '': value[1]===1? 'X':'O'}</button></td>
-                    <td><button className="block" onClick={() => {btnClick(2)}}>{value[2]===0? '': value[2]===1? 'X':'O'}</button></td>
-                </tr>
-                <tr>
-                    <td><button className="block" onClick={() => {btnClick(3)}}>{value[3]===0? '': value[3]===1? 'X':'O'}</button></td>
-                    <td><button className="block" onClick={() => {btnClick(4)}}>{value[4]===0? '': value[4]===1? 'X':'O'}</button></td>
-                    <td><button className="block" onClick={() => {btnClick(5)}}>{value[5]===0? '': value[5]===1? 'X':'O'}</button></td>
-                </tr>
-                <tr>
-                    <td><button className="block" onClick={() => {btnClick(6)}}>{value[6]===0? '': value[6]===1? 'X':'O'}</button></td>
-                    <td><button className="block" onClick={() => {btnClick(7)}}>{value[7]===0? '': value[7]===1? 'X':'O'}</button></td>
-                    <td><button className="block" onClick={() => {btnClick(8)}}>{value[8]===0? '': value[8]===1? 'X':'O'}</button></td>
-                </tr>
+                    <tr>
+                        <td><button className="block" onClick={() => {btnClick(0)}}>{value[0]===0? '': value[0]===1? 'X':'O'}</button></td>
+                        <td><button className="block" onClick={() => {btnClick(1)}}>{value[1]===0? '': value[1]===1? 'X':'O'}</button></td>
+                        <td><button className="block" onClick={() => {btnClick(2)}}>{value[2]===0? '': value[2]===1? 'X':'O'}</button></td>
+                    </tr>
+                    <tr>
+                        <td><button className="block" onClick={() => {btnClick(3)}}>{value[3]===0? '': value[3]===1? 'X':'O'}</button></td>
+                        <td><button className="block" onClick={() => {btnClick(4)}}>{value[4]===0? '': value[4]===1? 'X':'O'}</button></td>
+                        <td><button className="block" onClick={() => {btnClick(5)}}>{value[5]===0? '': value[5]===1? 'X':'O'}</button></td>
+                    </tr>
+                    <tr>
+                        <td><button className="block" onClick={() => {btnClick(6)}}>{value[6]===0? '': value[6]===1? 'X':'O'}</button></td>
+                        <td><button className="block" onClick={() => {btnClick(7)}}>{value[7]===0? '': value[7]===1? 'X':'O'}</button></td>
+                        <td><button className="block" onClick={() => {btnClick(8)}}>{value[8]===0? '': value[8]===1? 'X':'O'}</button></td>
+                    </tr>
                 </tbody>
             </table>
         </div>
---
+++ web-react/src/TicTacToe/component/Condition.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export const Condition = (count,choose) => {
+    /*
+    0 1 2
+    3 4 5
+    6 7 8
+     */
+    const victory = [[0,1,2],[3,4,5],[6,7,8],
+                     [0,3,6],[1,4,7],[2,5,8],
+                     [1,4,8],[2,4,6]];
+    // 승리 조건
+    for (let i=0;i<victory.length;i++){
+        let result = 0;
+        for (let j=0;j<3;j++) {
+            if (choose[victory[i][j]] === 1) {
+               result++;
+            }
+        }
+        if (result === 3) return "승리"
+    }
+
+    // 무승부 조건
+    if (count === 8) return "무승부"
+
+    // 그 외
+    return "continue";
+}
