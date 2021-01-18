7f3d5bfabe282e609c7641ca32dc09dc8a79cae3
Hyewon23 <ch2w2006@kookmin.ac.kr>
2021-01-18 22:52:37 +0900

    [TicTacToe]Reset & CSS

--- web-react/src/TicTacToe/component/Board.js
+++ web-react/src/TicTacToe/component/Board.js
@@ -8,57 +8,95 @@ const choose_O = [0,0,0,0,0,0,0,0,0];
 
 export const Board = (props) => {
     let result = '';
+    let finish = false;
 
     const [user, setUser] = useState({
-        player : 'X',
+        player : '다음은 X의 차례입니다',
         count : 0,
+        gameover: false,
     })
 
     const btnClick = (idx) => {
-        if (value[idx] === 0){
-            console.log("어떤 칸을 눌렀나요?");
-            // user X = 1, user O = 2를 부여
-            if (user.count % 2 === 0){
-                value[idx] = 1;
-                choose_X[idx] = 1;
+        if (!user.gameover){
+            if (value[idx] === 0){
+                console.log("어떤 칸을 눌렀나요?");
+                // user X = 1, user O = 2를 부여
+                if (user.count % 2 === 0){
+                    value[idx] = 1;
+                    choose_X[idx] = 1;
 
-                result = Condition(user.count, choose_X);
-                if (result === '승리') result = `축하합니다! ${user.player}가 승리했습니다!`;
-                else if (result === '무승부') result = "무승부입니다...아쉽네요...!";
-                else result = 'O'
+                    result = Condition(user.count, choose_X);
+                    if (result === '승리') {
+                        result = `축하합니다! X가 승리했습니다!`;
+                        finish = true;
+                    }
+                    else if (result === '무승부') {
+                        result = "무승부입니다...아쉽네요...!";
+                        finish = true;
+                    }
+                    else result = '다음은 O의 차례입니다'
 
-                setUser({
-                    player : result,
-                    count : user.count+1,
-                })
+                    setUser({
+                        player : result,
+                        count : user.count+1,
+                        gameover : finish,
+                    })
+                }
+
+                else{
+                    value[idx] = 2;
+                    choose_O[idx] = 1;
+
+                    result = Condition(user.count, choose_O);
+                    if (result === '승리') {
+                        result = `축하합니다! O가 승리했습니다!`;
+                        finish = true;
+                    }
+                    else if (result === '무승부') {
+                        result = "무승부입니다...아쉽네요...!";
+                        finish = true;
+                    }
+                    else result = '다음은 X의 차례입니다'
+
+                    setUser({
+                        player : result,
+                        count : user.count+1,
+                        gameover : finish,
+                    })
+                }
+                console.log(user.count, "번째: ", user.player,"가 ", idx, "번째 칸을 눌렀습니다!");
             }
-
             else{
-                value[idx] = 2;
-                choose_O[idx] = 1;
-
-                result = Condition(user.count, choose_O);
-                if (result === '승리') result = `축하합니다! ${user.player}가 승리했습니다!`
-                else if (result === '무승부') result = "무승부입니다...아쉽네요...!";
-                else result = 'X'
-
-                setUser({
-                    player : result,
-                    count : user.count+1,
-                })
+                console.log(idx,"번째 칸은 이미 누르셨습니다ㅜ-ㅜ");
             }
-            console.log(user.count, "번째: ", user.player,"가 ", idx, "번째 칸을 눌렀습니다!");
+            return value;
         }
-        else{
-            console.log(idx,"번째 칸은 이미 누르셨습니다ㅜ-ㅜ");
+        else {
+            alert("게임이 종료되었습니다!");
+            result = '';
+            finish = false;
         }
-        return value;
+    }
+
+    const btnReset = () => {
+        alert("reset");
+        console.log("reset");
+        for (let i=0;i<9;i++){
+            value[i] = 0;
+            choose_X[i] = 0;
+            choose_O[i] = 0;
+        }
+        setUser({
+            player : '다음은 X의 차례입니다',
+            count : 0,
+            gameover: false,
+        })
     }
 
     return <>
-        <div>{user.player}</div>
+        <div id="state">{user.player}</div>
         <div>
-            <table>
+            <table id = "board">
                 <tbody>
                     <tr>
                         <td><button className="block" onClick={() => {btnClick(0)}}>{value[0]===0? '': value[0]===1? 'X':'O'}</button></td>
@@ -78,6 +116,7 @@ export const Board = (props) => {
                 </tbody>
             </table>
         </div>
+        <div id="reset" ><button id="resetbtn" onClick={btnReset}>RESET</button></div>
     </>
 }
 
