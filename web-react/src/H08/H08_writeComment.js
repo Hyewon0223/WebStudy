89841a2ce591c0f76f76b2fe809a69099bdc02a2
Hyewon23 <ch2w2006@kookmin.ac.kr>
2021-01-27 21:31:02 +0900

    [H08_Commit]file

---
+++ web-react/src/H08/H08_writeComment.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Redirect} from "react-router-dom";
+
+import {LogInPage} from '../H07/page/LogInPage';
+import {JoinPage} from '../H07/page/JoinPage';
+import MainPage from "../H05/page/MainPage";
+import TLPage from "../H05/page/TimeLinePage";
+import MyPage from "../H05/page/MyPage";
+
+export const H07_Join_LogIn = () => {
+    return <>
+        <Router>
+            <div>
+                <Route exact path='/' component={LogInPage}/>
+                <Route path='/Join' component={JoinPage}/>
+                <Route path='/MainPage' component={MainPage}/>
+                <Route path='/TimeLine' component={TLPage}/>
+                <Route path='/MyPage' component={MyPage}/>
+            </div>
+        </Router>
+    </>
+
+}
+
+export default H07_Join_LogIn;
