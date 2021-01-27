5de6d9f5bf10b0740e209d8644e37bafadc18c15
Hyewon23 <ch2w2006@kookmin.ac.kr>
2021-01-27 21:43:29 +0900

    [H08_Commit]basic

--- web-react/src/H08/component/Comment.js
+++ web-react/src/H08/component/Comment.js
@@ -1,3 +1,10 @@
 import React, {useState, useEffect} from 'react';
 import './Comment.css';
 
+export const Comment = () => {
+    return <>
+        <div>Comment</div>
+    </>
+}
+
+export default Comment;
---
+++ web-react/src/H08/component/Post.js
@@ -0,0 +1,10 @@
+import React, {useState, useEffect} from 'react';
+import './Post.css';
+
+export const Post = () => {
+    return <>
+        <div>Post</div>
+    </>
+}
+
+export default Post;
